1df4f652f7aefde81fd42094ce9a7663907d453c
Madeleine <130216599+Madeleine12@users.noreply.github.com>
2023-08-21 16:24:10 +0200

    Update and rename parcours_automate.py to useful_methods.py

--- useful_methods.py
+++ useful_methods.py
@@ -1,8 +1,5 @@
 import pydot
 
-# auto = pydot.graph_from_dot_file('test.dot')
-# auto = auto[0]
-
 
 def path_between_two_states(graph_file, beginning, end):
     graph = pydot.graph_from_dot_file(graph_file)[0]
@@ -16,22 +13,7 @@ def path_between_two_states(graph_file, beginning, end):
             return path_between_two_states(graph_file, beginning, edge.get_source()) + [edge.get_label()]
 
 
-# def result_of_path(automaton_file, path):
-#     automaton = pydot.graph_from_dot_file(automaton_file)[0]
-#     source = 's0'
-#     for character in path :
-#         i = 0  
-#         stop = False
-#         while not stop :
-#             edge = automaton.get_edges()[i]
-#             if edge.get_label() == '\"'+character+'\"' and edge.get_source() == source:
-#                 source = edge.get_destination()
-#                 stop = True
-#             i += 1    
-#     return source
-
-
-def result_of_path2(automaton_file, path, input_al = ['i', 'w','<C-c>', '<C-g>', '<C-v>', 'c', ':', 'v', 'g', 'h', '<C-o>', 'r', '<Esc>', '<CR>']):
+def result_of_path(automaton_file, path, input_al = ['i', 'w','<C-c>', '<C-g>', '<C-v>', 'c', ':', 'v', 'g', 'h', '<C-o>', 'r', '<Esc>', '<CR>']):
     graph = pydot.graph_from_dot_file(automaton_file)[0]
     source = 's0'
     for character in path :
